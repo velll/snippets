0a8874c19b726c9e20dbf1d85d802127396eeb1f
Paul <paulpavelwhatever@gmail.com>
2013-10-25 17:18:27 +0300

    Using SQL collections in queries

---
+++ oracle/sql_collections_in_queries.sql
@@ -0,0 +1,19 @@
+create or replace type NUMBER_TABLE as TABLE OF NUMBER
+/
+declare
+  nt   NUMBER_TABLE := NUMBER_TABLE (1, 2, 3);
+  num_DUM  NUMBER;
+BEGIN
+  WITH d AS (
+    SELECT 1  DUM
+    FROM   DUAL)
+  SELECT  D.DUM
+  INTO    num_DUM
+  FROM    D,
+          TABLE(nt)   N
+  WHERE   D.DUM = N.COLUMN_VALUE;
+
+  RAISE_APPLICATION_ERROR(-20100, num_DUM);
+END;
+/
+-- raises exception with "1"
