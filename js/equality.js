c48d434e6bba2c3d1b6aa0e498e22a011162b633
Paul <paulpavelwhatever@gmail.com>
2015-02-07 18:39:21 +0300

    JS equality operators

---
+++ js/equality.js
@@ -0,0 +1,28 @@
+// a equal to b
+a == b
+
+// a equal to b AND type of a is equal to type of b
+a === b
+
+// Examples:
+
+0 == false
+// -> true
+
+0 === false
+// -> false
+
+
+// Negation
+
+// a is not equal to b
+a != b
+
+// a is not equal to b OR type of a is not equal to type of b
+a !== b
+
+0 != false
+// -> false (they are actually equal)
+
+0 !== false
+// -> true (they don't have the same type)
