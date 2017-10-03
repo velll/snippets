daaa0d1a259419bca900241c14c1652fdde022c7
Paul <paulpavelwhatever@gmail.com>
2017-10-03 19:28:24 +0300

    Using pipelined functions in oracle

---
+++ oracle/pipelined_functions.sql
@@ -0,0 +1,35 @@
+create table pipelined_table(
+  id   number,
+  name varchar2(256));
+
+create or replace package pipelined_package
+IS
+  type pipeline is table of pipelined_table%rowtype;
+
+  function pipelined_function(
+    num_Input                   IN NUMBER)
+  RETURN pipeline
+  PIPELINED;
+END pipelined_package;
+/
+create or replace package body pipelined_package
+IS
+  function pipelined_function(
+    num_Input                   IN NUMBER)
+  RETURN pipeline
+  PIPELINED
+  IS
+    rc_Pipe                     pipelined_table%ROWTYPE;
+  BEGIN
+    rc_Pipe.id := 1;
+    rc_Pipe.name := 'somebody';
+
+    PIPE ROW(rc_Pipe);
+
+    -- NO RETURN;
+  END pipelined_function;
+END pipelined_package;
+/
+-- Usage
+SELECT *
+FROM   TABLE(pipelined_package.pipelined_function(1));
