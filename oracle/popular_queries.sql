13067a1d0d9ac7b9e23a5712fc201bca91470d9b
Paul <paulpavelwhatever@gmail.com>
2017-12-27 15:37:13 +0300

    Select "popular" queries in Oracle

---
+++ oracle/popular_queries.sql
@@ -0,0 +1,13 @@
+with
+  session_querys AS (
+    select sql_id, count(*)  n_query_count
+    from v$session    s
+    where s.type = 'USER'
+    group by sql_id)
+select Q.sql_id,
+       q.n_query_count,
+       s.sql_text
+from   session_querys    Q,
+       v$sql             S
+where  s.sql_id = Q.sql_id
+order by q.n_query_count desc;
