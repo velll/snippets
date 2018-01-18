803edd4002b95bd4a96dc598ad2824630ead602b
Paul <paulpavelwhatever@gmail.com>
2018-01-18 14:11:33 +0300

    Script to drop all tables

---
+++ oracle/drop_tables.sql
@@ -0,0 +1,11 @@
+BEGIN
+  FOR rl_Tbl IN (
+    SELECT *
+    FROM   ALL_TABLES
+    WHERE  OWNER = :schema)
+  LOOP
+    EXECUTE IMMEDIATE '
+     DROP TABLE '|| :schema || '.' || rl_Tbl.TABLE_NAME;
+  END LOOP;
+END;
+/
