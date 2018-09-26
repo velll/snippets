73c8c681fbfb83041fc6fd4fb5537432b7444a2c
Paul <paulpavelwhatever@gmail.com>
2018-09-26 22:23:19 +0300

    Client info

---
+++ oracle/dev/client_info.sql
@@ -0,0 +1,13 @@
+-- set
+exec DBMS_APPLICATION_INFO.SET_CLIENT_INFO('some client string');
+
+DECLARE
+  client_info     V$SESSION.CLIENT_INFO%TYPE;
+BEGIN
+  -- read
+  DBMS_APPLICATION_INFO.SET_CLIENT_INFO(client_info);
+
+  -- do stuff
+  -- ...
+END;
+/
